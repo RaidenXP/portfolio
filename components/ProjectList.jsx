b3b54dfd85ddba71aa9cc969eb5788426ae048c8
RaidenXP <nathanthientran@gmail.com>
2023-08-11 04:00:44 -0700

    Progress on Projects page layout
    Finished the design of the projects page layout. However, functionality has to be implemented.

---
+++ components/ProjectList.jsx
@@ -0,0 +1,202 @@
+import Link from "next/link"
+
+const ProjectList = () => {
+  return (
+    <>
+      <ul className="block ms-0 me-0 m-0 p-0 list-none">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            Main Page
+          </Link>
+        </li>
+      </ul>
+      <ul className="block ms-0 me-0 m-0 p-0">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            Spotify Collaboration Web App
+          </Link>
+          <div className="h-auto opacity-[1] overflow-hidden flex flex-col">
+            <div className="relative">
+              <div className="absolute bottom-0 left-0 h-[6px] bg-gradient-to-t from-inherit w-full z-10 pointer-events-none"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul className="block ms-0 me-0 m-0 p-0">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            Sous Chef Mobile App
+          </Link>
+          <div className="h-auto opacity-[1] overflow-hidden flex flex-col">
+            <div className="relative">
+              <div className="absolute bottom-0 left-0 h-[6px] bg-gradient-to-t from-inherit w-full z-10 pointer-events-none"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul className="block ms-0 me-0 m-0 p-0">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            Training Tracker
+          </Link>
+          <div className="h-auto opacity-[1] overflow-hidden flex flex-col">
+            <div className="relative">
+              <div className="absolute bottom-0 left-0 h-[6px] bg-gradient-to-t from-inherit w-full z-10 pointer-events-none"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul className="block ms-0 me-0 m-0 p-0">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            Data Mining Series
+          </Link>
+          <div className="h-auto opacity-[1] overflow-hidden flex flex-col">
+            <div className="relative">
+              <ul className="mb-0 px-0.5 ">
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Overview
+                  </Link>
+                </li>
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Results
+                  </Link>
+                </li>
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Credits
+                  </Link>
+                </li>
+              </ul>
+              <div className="absolute bottom-0 left-0 h-[6px] bg-gradient-to-t from-inherit w-full z-10 pointer-events-none"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul className="block ms-0 me-0 m-0 p-0">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            Game AI Series
+          </Link>
+          <div className="h-auto opacity-[1] overflow-hidden flex flex-col">
+            <div className="relative">
+              <ul className="mb-0 px-0.5 ">
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Overview
+                  </Link>
+                </li>
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Results
+                  </Link>
+                </li>
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Credits
+                  </Link>
+                </li>
+              </ul>
+              <div className="absolute bottom-0 left-0 h-[6px] bg-gradient-to-t from-inherit w-full z-10 pointer-events-none"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul className="block ms-0 me-0 m-0 p-0">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            Raytracing Series
+          </Link>
+          <div className="h-auto opacity-[1] overflow-hidden flex flex-col">
+            <div className="relative">
+              <ul className="mb-0 px-0.5 ">
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Overview
+                  </Link>
+                </li>
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Results
+                  </Link>
+                </li>
+                <li className="my-1.5">
+                  <Link href='/' className="flex w-full relative cursor-pointer 
+                  items-center justify-between rounded-md pl-2 py-1 text-left
+                  text-sm hover:text-black text-gray-900">
+                    Credits
+                  </Link>
+                </li>
+              </ul>
+              <div className="absolute bottom-0 left-0 h-[6px] bg-gradient-to-t from-inherit w-full z-10 pointer-events-none"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul className="block ms-0 me-0 m-0 p-0">
+        <li className="my-1.5 ml-[3px]">
+          <Link
+            href='/Projects'
+            className="flex w-full relative cursor-pointer items-center justify-between rounded-md pl-2
+            py-1 text-left text-sm hover:text-black font-medium text-blue-700"
+          >
+            This Portfolio!
+          </Link>
+          <div className="h-auto opacity-[1] overflow-hidden flex flex-col">
+            <div className="relative">
+              <div className="absolute bottom-0 left-0 h-[6px] bg-gradient-to-t from-inherit w-full z-10 pointer-events-none"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default ProjectList
--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import ProjectList from './ProjectList';
 
 const SideBar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -25,7 +26,11 @@ const SideBar = () => {
             className={`${toggleMenu ? 'w-full h-screen block' : 'hidden'}`}
           >
             <div className="pb-[1px]">
-              <span>Project List</span>
+              <div className='flex items-center p-2 w-full h-[60px]'>
+                <h2 className='text-xl font-extrabold'>
+                  Project Box
+                </h2>
+              </div>
             </div>
             <div className="overflow-hidden relative">
               <nav
@@ -36,6 +41,7 @@ const SideBar = () => {
                 <div className="absolute bottom-0 left-0 h-3 bg-gradient-to-t from-inherit w-full z-10
                 pointer-events-none"></div>
                 {/**LIST OF PROJECTS HERE*/}
+                <ProjectList/>
               </nav>
             </div>
           </div>
@@ -47,18 +53,25 @@ const SideBar = () => {
         className="hidden md:flex w-[284px] md:shrink-0 sticky top-[121px] h-[calc(100vh-121px)]
         md:flex-col md:justify-between"
       >
+        <div className='pb-[1px] block'>
+          <div className='flex items-center p-2 h-[60px]'>
+            <h2 className='text-xl font-extrabold items-center'>
+              Project Box
+            </h2>
+          </div>
+        </div>
         <div
           className='overflow-hidden relative'
         >
           <nav
-            className='h-[calc(100vh-200px)] flex flex-col pr-2 pb-4'
+            className='h-[calc(100vh-200px)] overflow-y-scroll flex flex-col pr-2 pb-4'
           >
             <div className="absolute top-0 left-0 h-3 bg-gradient-to-b from-inherit w-full z-10
             pointer-events-none"></div>
             <div className="absolute bottom-0 left-0 h-3 bg-gradient-to-t from-inherit w-full z-10
             pointer-events-none"></div>
             {/**LIST OF PROJECTS HERE*/}
-            sidebar
+            <ProjectList/>
           </nav>
         </div>
       </div>
